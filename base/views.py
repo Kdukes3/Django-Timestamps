c0563195d8107a0f19f3954c0eb8904f95c64de9
Kdukes3 <dukeskyrin@gmail.com>
2025-02-22 13:40:41 -0500

    1:04:25 - Database & Admin Panel

--- base/views.py
+++ base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Home Page')
+rooms = [
+    {'id:1', 'name' : 'Lets learn python!'},
+    {'id:2', 'name' : 'Design with me'},
+    {'id:3', 'name' : 'Frontend developers'},
+]
 
-def room(request);
-    return HttpResponse('Room')
+def home(request):
+    context = {'rooms' : rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    return render(request, 'base/room.html')
