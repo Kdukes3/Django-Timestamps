d0427cf88d7287bdc4b86b02b340f37a14785e82
Kdukes3 <dukeskyrin@gmail.com>
2025-02-22 20:44:20 -0500

    2:32:25 - Flash Messages

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+
     path('',views.home, name="home"),
     path('room/<str:pk>/' , views.room, name="room"),
+
+    path('create-room/', view.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', view.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', view.deleteRoom, name="delete-room"),
 ]
